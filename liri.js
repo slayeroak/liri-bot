17c020ac85377c55c5894680224b590b8497811a
slayeroak <roakley1@villanova.edu>
2020-02-17 20:55:43 -0500

    initial commit, followed trilogy instructions

---
+++ liri.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+
+// concert-this
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
